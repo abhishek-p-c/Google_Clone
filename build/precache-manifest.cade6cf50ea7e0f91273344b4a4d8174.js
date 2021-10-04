856920d853a29c9ed869701572a3828a470819bd
abhishek-p-c <71756162+abhishek-p-c@users.noreply.github.com>
2021-10-04 16:52:01 +0530

    Add files via upload

---
+++ build/precache-manifest.cade6cf50ea7e0f91273344b4a4d8174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da754e5541badb212e144be83187ff8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b258de9893dfe4fa2461",
+    "url": "/static/css/main.7091c5f6.chunk.css"
+  },
+  {
+    "revision": "9c77880983d17bdd1e09",
+    "url": "/static/js/2.766cfa37.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.766cfa37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b258de9893dfe4fa2461",
+    "url": "/static/js/main.424cd422.chunk.js"
+  },
+  {
+    "revision": "42236b3c5a0e9054fb96",
+    "url": "/static/js/runtime-main.0c82e416.js"
+  }
+]);
